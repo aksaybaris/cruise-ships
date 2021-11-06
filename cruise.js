d813fba237407d8f8f81ed7867f80bd5f3ebb62c
baris_aksay <barish.aksay@gmail.com>
2021-11-06 21:44:18 +0200

    canSetSail method is added along with relevant tests

--- cruise.js
+++ cruise.js
@@ -1,5 +1,10 @@
-function Ship (startingPort){
-    this.startingPort=startingPort;
+class Ship{
+    constructor(startingPort){
+        this.startingPort=startingPort;
+    }
+    canSetSail(){
+        return this.startingPort="";
+    }
 };
 
 module.exports = Ship;
--- cruise.test.js
+++ cruise.test.js
@@ -6,9 +6,18 @@ describe("constructor have a property of starting dock",()=>{
         expect(new Ship).toBeInstanceOf(Object);
     });
 
-    it("should have a starting dock", ()=>{
+    it("should have a starting port", ()=>{
         let ship1 = new Ship("Girne");
         expect(ship1.startingPort).toBe("Girne");
 
     });
 });
+
+
+describe("starting port should change state to false", ()=>{
+    it("should change the port to falsy value", ()=>{
+        let ship1 = new Ship("Girne");
+        ship1.canSetSail();
+        expect(ship1.startingPort).toBeFalsy();
+    })
+})
