2061396bd002ef8bc35a24281265b2b84f02b366
baris_aksay <barish.aksay@gmail.com>
2021-11-14 20:34:44 +0200

    everything up to throw error

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -1,16 +1,19 @@
 /* globals describe it expect */
 const Ship = require("../ship");
 const Port = require("../port");
-/*const Itinerary = require("../itinerary");*/
+const Itinerary = require("../itinerary");
 
 
 describe("Ship object",()=>{
     let dover;
+    let myItinerary;
     let myShip; 
 
     beforeEach(()=>{
-        dover = new Port("Dover")
-        myShip = new Ship(dover);
+        dover = new Port("Dover");
+        myItinerary = new Itinerary([dover]);
+        myShip = new Ship(myItinerary);
+
     })
 
     it("can be instantiated", ()=>{
@@ -21,7 +24,13 @@ describe("Ship object",()=>{
         expect(myShip).hasOwnProperty("currentPort");
     })
 
+    it("has a starting port",()=>{
+        expect(myShip.currentPort).toBe(dover);
+    })
+
     it("should set sail",()=>{
+        let calais = new Port("calais")
+        myItinerary.ports.push(calais);
         myShip.setSail();
         expect(myShip.currentPort).toBeFalsy();
         expect(myShip.previousPort).toEqual(dover);
@@ -35,10 +44,29 @@ describe("Ship object",()=>{
         expect(myShip.currentPort).toBe(dover)
     })
 
+})
+
+describe("dock method",()=>{
+
+    let dover;
+    let calais;
+    let myItinerary;
+    let myShip;
+    beforeEach(()=>{
+        dover = new Port("dover");
+        calais = new Port("calais");
+        myItinerary = new Itinerary([dover,calais]);
+        myShip= new Ship(myItinerary);
+        myShip.setSail();
+        myShip.dock();
+    })
+
     it("should test the dock method",()=>{
-        let calais = new Port("calais");
-        myShip.dock(calais);
         expect(myShip.currentPort).toEqual(calais);
+    })
+
+    it("cant sail further than itinerary",()=>{
+        expect(()=>myShip.setSail()).toThrowError("End of tour");
     })
 
 })
